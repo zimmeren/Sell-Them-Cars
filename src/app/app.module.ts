604051b08796217948ae85921162daeccc9d9cc2
Austin Zimmer <zimmeren@umich.edu>
2017-05-07 14:03:33 -0500

    create responsive site framework using flexbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { ContentComponent } from './content/content.component';
+import { LocationComponent } from './location/location.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { PortalComponent } from './portal/portal.component';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavComponent,
+    ContentComponent,
+    LocationComponent,
+    FooterComponent,
+    ContactComponent,
+    AboutComponent,
+    PortalComponent,
+    ImageViewerComponent,
+    VehicleComponent
   ],
   imports: [
     BrowserModule,
