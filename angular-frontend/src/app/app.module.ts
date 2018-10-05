6527af7c53a1cc82022713ab2120d17180c5edeb
unknown <zimmeren@umich.edu>
2018-10-04 21:32:55 -0400

    Generate all needed components and services

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
+import { LocationComponent } from './location/location.component';
+import { PortalComponent } from './portal/portal.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderNavComponent,
+    ImageViewerComponent,
+    LocationComponent,
+    PortalComponent,
+    VehicleComponent,
+    VehicleListComponent
   ],
   imports: [
     BrowserModule
